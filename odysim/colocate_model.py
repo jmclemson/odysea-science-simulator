59448e44f739474c030f689be418af3840efe8ca
James Clemson <jclemson@iara>
2024-07-10 10:20:15 -0600

    Adds flag to colocate_model.GriddedModel.__init__ to read wind speed data and convert to wind stress

--- odysim/colocate_model.py
+++ odysim/colocate_model.py
@@ -25,9 +25,9 @@ class GriddedModel:
     
 
     def __init__(self,model_folder='/u/bura-m0/hectorg/COAS/llc2160/HighRes/',
-                 u_folder='U',v_folder='V',tau_x_folder='oceTAUX',tau_y_folder='oceTAUY',
-                 u_varname='U',v_varname='V',tau_x_varname='oceTAUX',tau_y_varname='oceTAUY',
-                 variable_selector='winds+currents',search_string = '/*.nc',preprocess=None,n_files=-1):
+                 u_folder='U',v_folder='V',wind_x_folder='oceTAUX',wind_y_folder='oceTAUY',
+                 u_varname='U',v_varname='V',wind_x_varname='oceTAUX',wind_y_varname='oceTAUY',
+                 variable_selector='winds+currents',wind_var = 'stress',search_string = '/*.nc',preprocess=None,n_files=-1):
 
         """
         Initialize a GriddedModel object.
@@ -69,17 +69,27 @@ class GriddedModel:
             self.v_varname = v_varname
 
         if 'wind' in variable_selector:
-            tau_x_search = os.path.join(model_folder, tau_x_folder)
-            tau_y_search = os.path.join(model_folder, tau_y_folder)
+            wind_x_search = os.path.join(model_folder, wind_x_folder)
+            wind_y_search = os.path.join(model_folder, wind_y_folder)
         
-            tau_x_files = np.sort(glob.glob(tau_x_search + search_string))[0:n_files]
-            tau_y_files = np.sort(glob.glob(tau_y_search + search_string))[0:n_files]
+            wind_x_files = np.sort(glob.glob(wind_x_search + search_string))[0:n_files]
+            wind_y_files = np.sort(glob.glob(wind_y_search + search_string))[0:n_files]
 
-            self.TX = xr.open_mfdataset(tau_x_files,parallel=True,preprocess=preprocess)
-            self.TY = xr.open_mfdataset(tau_y_files,parallel=True,preprocess=preprocess)
+            if 'speed' in wind_var:
+                u10 = xr.open_mfdataset(wind_x_files,parallel=True,preprocess=preprocess)
+                v10 = xr.open_mfdataset(wind_y_files,parallel=True,preprocess=preprocess)
 
-            self.tau_x_varname = tau_x_varname
-            self.tau_y_varname = tau_y_varname
+                wind_speed = np.sqrt(u10**2 + v10**2)
+                wind_dir = np.arctan2(u10, v10)
+
+                self.TX, self.TY = utils.windToStress(wind_speed, wind_dir)
+
+            else:
+                self.TX = xr.open_mfdataset(wind_x_files,parallel=True,preprocess=preprocess)
+                self.TY = xr.open_mfdataset(wind_y_files,parallel=True,preprocess=preprocess)
+
+                self.tau_x_varname = wind_x_varname
+                self.tau_y_varname = wind_y_varname
 
         
         
